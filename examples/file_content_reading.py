89d176d6d9802570eafbf42997efbf32c09c02b1
GizmoBricks <gizmobrickschannel@gmail.com>
2023-12-20 12:44:35 +0100

    Minor fixes.

--- examples/file_content_reading.py
+++ examples/file_content_reading.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
         next(file)  # Skip the line with file information.
 
         for line in file:
-            # Try to convert the line in 'utf-8' format and print it.
+            # Try to convert the line into 'utf-8' format and print it.
             try:
                 print(str(line.rstrip(), 'utf-8'))
             # If line can't be decoded - skip this line.
