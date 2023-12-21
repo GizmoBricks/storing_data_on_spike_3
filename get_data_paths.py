958d0b4f3a05d3d0243440de93fd254eae8a6587
GizmoBricks <127412675+GizmoBricks@users.noreply.github.com>
2023-12-21 23:17:09 +0100

    Create get_data_paths.py

---
+++ get_data_paths.py
@@ -0,0 +1,21 @@
+def get_data_paths(do_check: bool = False, check_word: str = '') -> dict:
+    paths = {}
+    for slot in range(20):
+        path = '/flash/program/{:02}/program.mpy'.format(slot)
+        
+        try:
+            with open(path, 'rb') as test_file:
+                if b'__doc__' not in test_file.readline():
+                    continue
+
+                line = str(test_file.readline(), 'utf-8')
+
+        except (OSError, UnicodeError) as _:
+            continue
+        
+        if do_check and line.split()[0] != check_word:
+            continue
+
+        paths[slot] = path
+    
+    return paths
