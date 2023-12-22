d598748142f0105eba3f45864f5d1a60333d2464
GizmoBricks <127412675+GizmoBricks@users.noreply.github.com>
2023-12-22 17:26:42 +0100

    Update get_data_paths.py

--- get_data_paths.py
+++ get_data_paths.py
@@ -1,4 +1,30 @@
 def get_data_paths(do_check: bool = False, check_word: str = '') -> dict:
+    """
+    This function retrieves the paths associated with available slots
+    with files which contain initial docstring.
+    Function ignores slots with files which does not contain 
+    initial docstring.
+
+    Args:
+    - do_check (bool, optional): Flag to indicate whether to perform
+                                a file format check (default: False).
+    - check_word (str, optional): The word used for file format checking
+                                (default: empty string).
+
+    Returns:
+    - dict: The dictionary of available slots and their paths,
+            or empty dictionary, if no available slots.
+
+    File format check:
+    If the do_check argument is True, the function compares
+    the first word of the file with check_word.
+    If they match, the test is passed.
+    If they are different, that slot-path pair is excluded
+    from the dictionary.
+
+    Note: this function does not work with SPIKE Legacy or
+    Mindstorms Robot Inventor.
+    """
     paths_dict = {}
     for slot in range(20):
         path = '/flash/program/{:02}/program.mpy'.format(slot)
